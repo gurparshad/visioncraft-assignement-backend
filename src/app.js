b535d1726e5908b8c898dbf3f9912a915578c53f
gurparshad <panksharma230@gmail.com>
2021-02-22 19:51:31 +0200

    enable cors

--- src/app.js
+++ src/app.js
@@ -2,8 +2,9 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const UserRouter = require("./user/UserRouter");
+const cors = require("cors");
 require("dotenv").config();
-
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
